f3881f92f9b394fed7d681765be9a9a7fc0dedb6
mailaw <mlaw534@gmail.com>
2018-10-16 19:04:26 -0400

    relationship rendering transparent

--- 3d-browser/js/render.js
+++ 3d-browser/js/render.js
@@ -73,7 +73,13 @@ function readRelationData(results) {
     geometry.faces.push( new THREE.Face3(3,2,0) );
     geometry.computeBoundingSphere();
 
-    var planeMaterial = new THREE.MeshBasicMaterial();
+    var planeMaterial = new THREE.MeshLambertMaterial( {
+						color: 0x1cff44,
+						emissive: 0x1cff44,
+                        transparent: true,
+                        opacity: 0.6,
+        
+					} );
     var plane = new THREE.Mesh(geometry, planeMaterial);
     //plane.position.x = (entity_row.x1+entity_row.x3)/2;
     //plane.position.z = (entity_row.y1+entity_row.y3)/2;
