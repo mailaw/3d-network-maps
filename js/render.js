db5f171710c0d08f1089706222d3178254f1d24e
mailaw <mlaw534@gmail.com>
2018-10-24 16:50:43 -0400

    added gui and test cute for opacity

--- js/render.js
+++ js/render.js
@@ -1,23 +1,66 @@
-// must have scene, camera, renderer
-var scene = new THREE.Scene();
-var aspect = window.innerWidth / window.innerHeight;
+var camera, scene, renderer, local_canvas, controls;
+var mesh, color;
+function init() {
+  // must have scene, camera, renderer
+  scene = new THREE.Scene();
+  var aspect = window.innerWidth / window.innerHeight;
 
-var local_canvas = document.getElementById("vis-window");
+  local_canvas = document.getElementById("vis-window");
 
-//field of view, aspect ratio, near & far clipping plane
-var camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
+  //field of view, aspect ratio, near & far clipping plane
+  camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
 
-//orbital controls
-var controls = new THREE.OrbitControls( camera, local_canvas);
+  //orbital controls
+  controls = new THREE.OrbitControls(camera, local_canvas);
 
-//This can be swapped out later for VR
-var renderer = new THREE.WebGLRenderer({canvas:local_canvas});
+  //This can be swapped out later for VR
+  renderer = new THREE.WebGLRenderer({ canvas: local_canvas });
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  //presets for variables that GUI will cchange
+  var options = {
+    material: {
+      opacity: 0.8,
+      transparent: true
+    },
+    radiusTop: 0.1,
+    radiusBottom: 0.1,
+    boxOpacity: 0.8
+  };
+
+  //Creating the dat.GUI
+  gui = new dat.GUI();
+
+  var cylinderControl = gui.addFolder("Cylinders");
+  cylinderControl
+    .add(options, "boxOpacity", 0, 1)
+    .name("Opacity")
+    .onChange(function() {
+      cubeMaterial.opacity = options.boxOpacity;
+      //options.boxOpacity = this.opacity;
+      console.log("hey opacity", options.boxOpacity);
+    });
+
+  /* cylinderControl
+  .add(options.material, "opacity", 0, 1)
+  .name("OpacityWListen")
+  .listen(); */
+
+  //test cube
+  var cubeGeometry = new THREE.CubeGeometry(100, 100, 100);
+  var cubeMaterial = new THREE.MeshLambertMaterial({
+    color: 0x1cff44,
+    emissive: 0x1cff44,
+    transparent: true,
+    opacity: options.boxOpacity
+  });
+  var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  scene.add(cube);
+}
 
 var data;
-
 function readEntityData(results) {
   data = results["data"];
 
@@ -37,57 +80,53 @@ function readEntityData(results) {
     cylinder.position.y = (entity_row.z2 - entity_row.z1) / 2 + entity_row.z1;
     cylinder.position.z = entity_row.y;
     scene.add(cylinder);
-//
-//    //add planes
-//    var plane = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
-//    scene.add(plane);
   }
-
 }
 
 function readRelationData(results) {
-    data = results["data"];
+  data = results["data"];
 
-    for (let index = 0; index < data.length; index++) {
+  for (let index = 0; index < data.length; index++) {
     var entity_row = data[index];
 
     //render planes
-    var cylinderWidth = Math.sqrt(Math.pow(entity_row.x3-entity_row.x1,2)+Math.pow(entity_row.y3-entity_row.y1,2));
-    var cylinderHeight = entity_row.z2-entity_row.z1;
+    var cylinderWidth = Math.sqrt(
+      Math.pow(entity_row.x3 - entity_row.x1, 2) +
+        Math.pow(entity_row.y3 - entity_row.y1, 2)
+    );
+    var cylinderHeight = entity_row.z2 - entity_row.z1;
     var planeGeometry = new THREE.PlaneBufferGeometry(
-        cylinderWidth,
-        cylinderHeight
+      cylinderWidth,
+      cylinderHeight
     );
 
     var geometry = new THREE.Geometry();
 
     geometry.vertices.push(
-    	new THREE.Vector3( entity_row.x1,  entity_row.z1, entity_row.y1),
-    	new THREE.Vector3( entity_row.x2,  entity_row.z2, entity_row.y2),
-    	new THREE.Vector3( entity_row.x4,  entity_row.z4, entity_row.y4),
-      new THREE.Vector3( entity_row.x3,  entity_row.z3, entity_row.y3)
+      new THREE.Vector3(entity_row.x1, entity_row.z1, entity_row.y1),
+      new THREE.Vector3(entity_row.x2, entity_row.z2, entity_row.y2),
+      new THREE.Vector3(entity_row.x4, entity_row.z4, entity_row.y4),
+      new THREE.Vector3(entity_row.x3, entity_row.z3, entity_row.y3)
     );
-    geometry.faces.push( new THREE.Face3(0,1,2) );
-    geometry.faces.push( new THREE.Face3(0,2,3) );
-    geometry.faces.push( new THREE.Face3(2,1,0) );
-    geometry.faces.push( new THREE.Face3(3,2,0) );
+    geometry.faces.push(new THREE.Face3(0, 1, 2));
+    geometry.faces.push(new THREE.Face3(0, 2, 3));
+    geometry.faces.push(new THREE.Face3(2, 1, 0));
+    geometry.faces.push(new THREE.Face3(3, 2, 0));
     geometry.computeBoundingSphere();
 
-    var planeMaterial = new THREE.MeshLambertMaterial( {
-						color: 0x1cff44,
-						emissive: 0x1cff44,
-                        transparent: true,
-                        opacity: 0.6,
-        
-					} );
+    var planeMaterial = new THREE.MeshLambertMaterial({
+      color: 0x1cff44,
+      emissive: 0x1cff44,
+      transparent: true,
+      opacity: 0.6
+    });
     var plane = new THREE.Mesh(geometry, planeMaterial);
     //plane.position.x = (entity_row.x1+entity_row.x3)/2;
     //plane.position.z = (entity_row.y1+entity_row.y3)/2;
     //plane.position.y = cylinderHeight/2;
     //plane.rotation.y = Math.atan((entity_row.z3 - entity_row.z1)/(entity_row.x3 - entity_row.x1));
     scene.add(plane);
-    }
-
+  }
 }
 
 function handleEntityFileSelect(evt) {
@@ -115,18 +154,20 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-   $("#relationship-csv-file").change(handleRelationFileSelect);
- });
+  $("#relationship-csv-file").change(handleRelationFileSelect);
+});
+
+init();
 
 //camera is automatically put at 0,0,0 so this brings it out from where the cube is
-camera.position.set(120, 15, 5,0,0,100);
+camera.position.set(120, 15, 5, 0, 0, 100);
 camera.lookAt(scene.position);
 controls.update();
 
 var animate = function() {
   requestAnimationFrame(animate);
 
-  var speed = Date.now() * 0.0005 / 2;
+  //var speed = (Date.now() * 0.0005) / 2;
   //camera.position.x = Math.cos(speed) * 10;
   //camera.position.z = Math.sin(speed) * 10;
 
