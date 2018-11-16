502d0d8e4dce031a03c53e8226fc53ddc6df1969
mailaw <mlaw534@gmail.com>
2018-11-16 17:02:06 -0500

    added info display for entities

--- js/render.js
+++ js/render.js
@@ -28,6 +28,13 @@ var parameters = {
     resetRadius();
   }
 };
+
+var projector, mouse = {
+  x: 0,
+  y: 0
+},
+INTERSECTED;
+
 var gui;
 var data;
 
@@ -79,6 +86,9 @@ function readEntityData(results) {
     cylinder.position.x = entity_row.x;
     cylinder.position.y = (entity_row.z2 - entity_row.z1) / 2 + entity_row.z1;
     cylinder.position.z = entity_row.y;
+    cylinder.name = entity_row.name;
+    cylinder.starting_date = entity_row.starting_date;
+    cylinder.ending_date = entity_row.ending_date;
     cylinder_postion_list.push([
       cylinder.position.x,
       cylinder.position.y,
@@ -180,7 +190,7 @@ function init() {
 
   // Orbital Controls
   controls = new THREE.OrbitControls(camera, local_canvas);
-  controls.autoRotate = true;
+  //controls.autoRotate = true;
   //RENDERER
   //This can be swapped out later for VR
   renderer = new THREE.WebGLRenderer({ canvas: local_canvas });
@@ -190,6 +200,10 @@ function init() {
   renderer.shadowMap.type = THREE.BasicShadowMap;
 
   document.body.appendChild(renderer.domElement);
+  
+  //MOUSE
+  document.addEventListener('mousemove', onDocumentMouseMove, false);
+  window.addEventListener('resize', onWindowResize, false);
 
   // LIGHTS
   /*helper to see direction of light
@@ -207,6 +221,8 @@ function init() {
   camera.add(light);
   //add light to follow the camera 
   scene.add( camera );
+
+
 
   //Client requested to be commented out & left for future dev
   /* GROUND
@@ -227,6 +243,17 @@ function init() {
   gui.domElement.id = "gui-position";
   var customContainer = document.getElementById("gui-position");
   customContainer.appendChild(gui.domElement);
+  /*
+  var name_folder = gui.addFolder("Name of Entity");
+  entityName = name_folder.add(parameters, "name");
+  // intersects[0].object.name
+  entityName.onChange(function(value) {
+    for (let i = 0; i < cylinder_list.length; i++) {
+      //cylinder_list[i].intersects[0].object.name = 
+    
+    }
+  });*/
+
 
   var planeFolder = gui.addFolder("Planes");
 
@@ -238,6 +265,7 @@ function init() {
     .name("Opacity")
     .listen();
   planeFolder.open();
+
 
   var cylinderFolder = gui.addFolder("Cylinders");
 
@@ -316,8 +344,6 @@ function init() {
       );
     }
   });
-
-  
 
   var globalFolder = gui.addFolder("Global Options");
 
@@ -427,4 +453,62 @@ function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
   controls.update();
+  update();
+}
+
+function onDocumentMouseMove(event) {
+  // the following line would stop any other event handler from firing
+  // (such as the mouse's TrackballControls)
+  // event.preventDefault();
+   // update the mouse variable
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
+
+ function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function update() {
+  // find intersections
+   // create a Ray with origin at the mouse position
+  //   and direction into the scene (camera direction)
+  camera.updateMatrixWorld();
+  var vector = new THREE.Vector3(mouse.x, mouse.y, 1);
+  vector.unproject(camera);
+  var ray = new THREE.Raycaster(camera.position, vector.sub(camera.position).normalize());
+   // create an array containing all objects in the scene with which the ray intersects
+  var intersects = ray.intersectObjects(scene.children);
+   // INTERSECTED = the object in the scene currently closest to the camera
+  //    and intersected by the Ray projected from the mouse position
+   // if there is one (or more) intersections
+  if (intersects.length > 0) {
+    // if the closest object intersected is not the currently stored intersection object
+    if (intersects[0].object != INTERSECTED) {
+      // restore previous intersection object (if it exists) to its original color
+      if (INTERSECTED)
+//        INTERSECTED.material.color.setHex(INTERSECTED.currentHex);
+//      // store reference to closest object as current intersection object
+      INTERSECTED = intersects[0].object;
+//      // store color of closest object (for later restoration)
+//      INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+//      // set a new color for closest object
+//      INTERSECTED.material.color.setHex(0xffff00);
+      //console.log("name:"+intersects[0].object.name);
+      document.getElementById("display_name").innerHTML = intersects[0].object.name;
+      document.getElementById("display_starting").innerHTML = intersects[0].object.starting_date;
+      document.getElementById("display_ending").innerHTML = intersects[0].object.ending_date;
+    }
+  } else // there are no intersections
+  {
+    // restore previous intersection object (if it exists) to its original color
+    if (INTERSECTED)
+      INTERSECTED.material.color.setHex(INTERSECTED.currentHex);
+    // remove previous intersection object reference
+    //     by setting current intersection object to "nothing"
+    INTERSECTED = null;
+  }
+    controls.update();
 }
