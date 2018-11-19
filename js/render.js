4be223d61849f80b4bfb7dd4e8571f902ecc204b
mailaw <mlaw534@gmail.com>
2018-11-19 13:28:51 -0500

    added hover

--- js/render.js
+++ js/render.js
@@ -35,6 +35,12 @@ var parameters = {
     resetRadius();
   }
 };
+
+var projector, mouse = {
+  x: 0,
+  y: 0
+},
+INTERSECTED;
 var gui;
 var data;
 
@@ -88,6 +94,11 @@ function readEntityData(results) {
     cylinder.position.x = entity_row.x;
     cylinder.position.y = (entity_row.z2 - entity_row.z1)*5 + entity_row.z1*10;
     cylinder.position.z = entity_row.y;
+
+    cylinder.name = entity_row.name;
+    cylinder.starting_date = entity_row.starting_date;
+    cylinder.ending_date = entity_row.ending_date;
+
     cylinder_postion_list.push([
       cylinder.position.x,
       cylinder.position.y,
@@ -164,24 +175,24 @@ function readRelationData(results) {
     var geometry = new THREE.Geometry();
 
     geometry.vertices.push(
-      new THREE.Vector3(entity_row.x1, entity_row.z1, entity_row.y1),
-      new THREE.Vector3(entity_row.x2, entity_row.z2, entity_row.y2),
-      new THREE.Vector3(entity_row.x4, entity_row.z4, entity_row.y4),
-      new THREE.Vector3(entity_row.x3, entity_row.z3, entity_row.y3)
+      new THREE.Vector3(entity_row.x1, entity_row.z1*10, entity_row.y1),
+      new THREE.Vector3(entity_row.x2, entity_row.z2*10, entity_row.y2),
+      new THREE.Vector3(entity_row.x4, entity_row.z4*10, entity_row.y4),
+      new THREE.Vector3(entity_row.x3, entity_row.z3*10, entity_row.y3)
     );
 
     plane_position_list.push([
       entity_row.x1,
-      entity_row.z1,
+      entity_row.z1*10,
       entity_row.y1,
       entity_row.x2,
-      entity_row.z2,
+      entity_row.z2*10,
       entity_row.y2,
       entity_row.x4,
-      entity_row.z4,
+      entity_row.z4*10,
       entity_row.y4,
       entity_row.x3,
-      entity_row.z3,
+      entity_row.z3*10,
       entity_row.y3
     ]);
 
@@ -236,6 +247,10 @@ function init() {
   renderer.shadowMap.type = THREE.BasicShadowMap;
 
   document.body.appendChild(renderer.domElement);
+
+  //MOUSE
+  document.addEventListener('mousemove', onDocumentMouseMove, false);
+  window.addEventListener('resize', onWindowResize, false);
 
   // LIGHTS
   /*helper to see direction of light
@@ -471,4 +486,59 @@ function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
   controls.update();
+  update();
+}
+ function onDocumentMouseMove(event) {
+  // the following line would stop any other event handler from firing
+  // (such as the mouse's TrackballControls)
+  // event.preventDefault();
+   // update the mouse variable
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+ function update() {
+  // find intersections
+   // create a Ray with origin at the mouse position
+  //   and direction into the scene (camera direction)
+  camera.updateMatrixWorld();
+  var vector = new THREE.Vector3(mouse.x, mouse.y, 1);
+  vector.unproject(camera);
+  var ray = new THREE.Raycaster(camera.position, vector.sub(camera.position).normalize());
+   // create an array containing all objects in the scene with which the ray intersects
+  var intersects = ray.intersectObjects(scene.children);
+   // INTERSECTED = the object in the scene currently closest to the camera
+  //    and intersected by the Ray projected from the mouse position
+   // if there is one (or more) intersections
+  if (intersects.length > 0) {
+    // if the closest object intersected is not the currently stored intersection object
+    if (intersects[0].object != INTERSECTED) {
+      // restore previous intersection object (if it exists) to its original color
+      if (INTERSECTED)
+//        INTERSECTED.material.color.setHex(INTERSECTED.currentHex);
+//      // store reference to closest object as current intersection object
+      INTERSECTED = intersects[0].object;
+//      // store color of closest object (for later restoration)
+//      INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+//      // set a new color for closest object
+//      INTERSECTED.material.color.setHex(0xffff00);
+      //console.log("name:"+intersects[0].object.name);
+      document.getElementById("display_name").innerHTML = intersects[0].object.name;
+      document.getElementById("display_starting").innerHTML = intersects[0].object.starting_date;
+      document.getElementById("display_ending").innerHTML = intersects[0].object.ending_date;
+    }
+  } else // there are no intersections
+  {
+    // restore previous intersection object (if it exists) to its original color
+    if (INTERSECTED)
+      INTERSECTED.material.color.setHex(INTERSECTED.currentHex);
+    // remove previous intersection object reference
+    //     by setting current intersection object to "nothing"
+    INTERSECTED = null;
+  }
+    controls.update();
 }
